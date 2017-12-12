d0b92c60d26dd6dd8a3986b8e16db7a8b2ad1702
Victor Coutinho <victormelo@id.uff.br>
2017-12-12 19:36:10 -0200

    Update flask-mail.py

--- flask-mail.py
+++ flask-mail.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'victormelo@id.uff.br'
-app.config['MAIL_PASSWORD'] = 'cou3707tinho'
+app.config['MAIL_PASSWORD'] = '******'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
