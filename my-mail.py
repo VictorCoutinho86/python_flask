db27d7d7bd75ae1e23015389b5556bfe23bc59ff
VictorCoutinho86 <victormelo@id.uff.br>
2017-12-13 21:04:35 -0200

    Alterando o nome

--- my-mail.py
+++ my-mail.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'victormelo@id.uff.br'
-app.config['MAIL_PASSWORD'] = '******'
+app.config['MAIL_PASSWORD'] = '*******'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
@@ -18,10 +18,11 @@ def index():
     msg = Message(
         'Hello',
         sender='victormelo@id.uff.br',
-        recipients=['victormelo@id.uff.br', 'fonsa19@hotmail.com'])
+        recipients=['victormelo@id.uff.br'])
     msg.body = "Hello From my first Flask Mail sent!"
     mail.send(msg)
     return "Sent"
+
 
 
 if __name__ == '__main__':
